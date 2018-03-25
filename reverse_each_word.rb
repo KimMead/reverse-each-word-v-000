7d98a6df73f0d007dff9afe09e03a7f22c5cdcbb
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-25 14:38:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,7 @@
 def reverse_each_word(string)
   string = ["Hello there, and how are you?"]
-  string.join(" ")
+  reversed = sentence.split(" ").each do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
